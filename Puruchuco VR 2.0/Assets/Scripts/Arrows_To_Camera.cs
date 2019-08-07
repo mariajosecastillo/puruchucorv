8dce56759db606cdeafcb2c8eac731192f26d80a
Mariajose <mariajose_castillo@usmp.pe>
2019-08-07 13:41:51 -0500

    Version 2

---
+++ Puruchuco VR 2.0/Assets/Scripts/Arrows_To_Camera.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrows_To_Camera : MonoBehaviour
+{
+    public float smooth = 2.0F;
+
+    public Transform ToCamera;
+
+    private bool MoveArrows = false;
+    void Update()
+    {
+        if (Mathf.Abs(ToCamera.rotation.y - transform.rotation.y) > 0.125)
+            MoveArrows = true;
+
+        if (transform.rotation == ToCamera.rotation)
+            MoveArrows = false;
+
+        if (MoveArrows)
+            transform.rotation = Quaternion.Slerp(ToCamera.rotation, transform.rotation, Time.deltaTime * smooth);
+
+    }
+}
---
+++ Puruchuco VR 2.0/Assets/Scripts/VR/GazePointer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GazePointer : MonoBehaviour
+{
+    void FixedUpdate()
+    {
+        LayerMask layerMask = ~(1 << LayerMask.NameToLayer("Ignore Raycast"));
+
+        RaycastHit hit;
+        // Does the ray intersect any objects excluding the player layer
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, 15, layerMask))
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+            if (hit.transform.tag == "Sound")
+            {
+                if (!hit.transform.GetComponent<AudioSource>().isPlaying)
+                    hit.transform.GetComponent<AudioSource>().Play();
+            }
+        }
+    }
+}
